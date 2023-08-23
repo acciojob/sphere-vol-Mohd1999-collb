77ebe51cf97cf1cbd353ba419774b2886f5ca460
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-24 00:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const radius = document.getElementById("radius");
+const volume = document.getElementById("volume");
+
 function volume_sphere() {
     //Write your code here
+	const vol_sphere = parseInt(4/3 * 22/7 * Math.pow(radius.value, 3));
+    volume.value = vol_sphere;
+    console.log(vol_sphere);
   
 } 
 
