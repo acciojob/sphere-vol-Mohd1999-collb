47a50dc0f244a4d41527b8130bed8a9bdaea84d4
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-24 00:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const volume = document.getElementById("volume");
 function volume_sphere() {
     //Write your code here
 	const vol_sphere = parseInt(4/3 * 22/7 * Math.pow(radius.value, 3));
-    volume.value = vol_sphere;
-    console.log(vol_sphere);
-  
+    volume.value = vol_sphere;  
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
